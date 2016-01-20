67900c16487a8ee15d96700998e41368152550d8
Daniel on umini <daniel@s.natur-kultur.eu>
2016-01-20 16:55:49 +0100

    progress

--- main.php
+++ main.php
@@ -1,3 +1,4 @@
 <?php
 require "config.php";
-function mts($h,$m,$s){return $h*60*60+$m*60+$s;}
+function mts($h,$m,$s){return $h*60*60+$m*60+$s;} // make time stamp
+function ftime($ts){return date("H:i:s",$ts);} // formatted time
